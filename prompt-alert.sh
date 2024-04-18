8d050219426aa1d9cd7e287bedb4c5f4b1db11bb
Jacob Garby <garby@chalmers.se>
2024-04-18 08:59:15 +0200

    use PROMPT_COMMAND to show warning instead of PS1

--- prompt-alert.sh
+++ prompt-alert.sh
@@ -1,2 +1,8 @@
-_RESMAN_WORD="server in use..."
-PS1='$(if resman; then echo; else printf "\[\033[0;31m\](${_RESMAN_WORD})\[\033[0m\] "; fi)'"${PS1}"
+function _resman_prompt_command {
+    if ! resman; then
+        printf "\033[0;31m(${_RESMAN_WORD})\033[0m ";
+    fi
+}
+
+PROMPT_COMMAND=_resman_prompt_command
+_RESMAN_WORD="server in use!"
