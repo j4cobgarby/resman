bd9461a2618c9d6978d3287646aa3b44c7fa74bd
Martin Hilgendorf <martin.hilgendorf@chalmers.se>
2026-06-22 16:52:09 +0200

    server: handle_client: Switch to using a switch to switch request handling

--- server/ipc.c
+++ server/ipc.c
@@ -74,106 +74,109 @@ int handle_client(int soc_client) { /*{{{*/
         return -1;
     }
 
-    if (req.req_type == IPCREQ_JOB) {
-        job_descriptor job = req.job;
+    switch (req.req_type) {
+        case IPCREQ_JOB:
+            job_descriptor job = req.job;
 
-        if (job.job_type != JOB_CMD && job.job_type != JOB_TIMESLOT) {
-            RESMAND_ERROR("Invalid job type in request\n");
-            goto _close;
-        }
+            if (job.job_type != JOB_CMD && job.job_type != JOB_TIMESLOT) {
+                RESMAND_ERROR("Invalid job type in request\n");
+                goto _close;
+            }
 
-        job.job_uuid = next_uuid();
+            job.job_uuid = next_uuid();
 
-        switch (job.job_type) {
-            case JOB_CMD:
-                RESMAND_INFO(
-                    "Received command job %d by user %d (pid: %d, msg: '%s')\n",
-                    job.job_uuid, job.uid, job.cmd.pid, job.msg
-                );
-                pthread_mutex_lock(&mut_q);
-                enq_job(&q, job);
-                pthread_mutex_unlock(&mut_q);
-                resp.status = STATUS_OK;
-                break;
-            case JOB_TIMESLOT:
-                RESMAND_INFO(
-                    "Received timeslot job %d by user %d (time: %ds, msg: '%s')\n",
-                    job.job_uuid, job.uid, job.timeslot.secs, job.msg
-                );
-
-                if (running_job || peek_job(q, 0)) {
+            switch (job.job_type) {
+                case JOB_CMD:
                     RESMAND_INFO(
-                        "Rejecting timeslot job %d: server is already reserved\n",
-                        job.job_uuid
+                        "Received command job %d by user %d (pid: %d, msg: '%s')\n",
+                        job.job_uuid, job.uid, job.cmd.pid, job.msg
                     );
-                    resp.status = STATUS_FAIL;
+                    pthread_mutex_lock(&mut_q);
+                    enq_job(&q, job);
+                    pthread_mutex_unlock(&mut_q);
+                    resp.status = STATUS_OK;
                     break;
-                }
+                case JOB_TIMESLOT:
+                    RESMAND_INFO(
+                        "Received timeslot job %d by user %d (time: %ds, msg: '%s')\n",
+                        job.job_uuid, job.uid, job.timeslot.secs, job.msg
+                    );
 
-                pthread_mutex_lock(&mut_q);
-                enq_job(&q, job);
-                pthread_mutex_unlock(&mut_q);
-                resp.status = STATUS_OK;
-                break;
-        }
+                    if (running_job || peek_job(q, 0)) {
+                        RESMAND_INFO(
+                            "Rejecting timeslot job %d: server is already reserved\n",
+                            job.job_uuid
+                        );
+                        resp.status = STATUS_FAIL;
+                        break;
+                    }
 
-        send_status(soc_client, &resp);
-        disp_status();
-    } else if (req.req_type == IPCREQ_VIEW_QUEUE) {
-        info_request info = req.info;
-        send_queue_info(soc_client, info.n_view);
-    } else if (req.req_type == IPCREQ_DEQUEUE) {
-        dequeue_request deq = req.deq;
-        RESMAND_INFO("Received dequeue request for job %d\n", deq.job_uuid);
+                    pthread_mutex_lock(&mut_q);
+                    enq_job(&q, job);
+                    pthread_mutex_unlock(&mut_q);
+                    resp.status = STATUS_OK;
+                    break;
+            }
 
-        pthread_mutex_lock(&mut_rj);
-        pthread_mutex_lock(&mut_q);
+            send_status(soc_client, &resp);
+            break;
+        case IPCREQ_VIEW_QUEUE:
+            info_request info = req.info;
+            send_queue_info(soc_client, info.n_view);
+            break;
+        case IPCREQ_DEQUEUE:
+            dequeue_request deq = req.deq;
+            RESMAND_INFO("Received dequeue request for job %d\n", deq.job_uuid);
 
-        if (running_job && running_job->job.job_uuid == deq.job_uuid) {
-            RESMAND_ERROR("Refusing to dequeue currently running job\n")
-            resp.status = STATUS_DEQ_FAIL_JOB_CURRENTLY_RUNNING;
-        } else {
-            queued_job *deq_job = remove_job(&q, deq.job_uuid);
-            if (!deq_job) {
-                RESMAND_ERROR("Failed to dequeue job %d: not found in queue"
-                              "\n", deq.job_uuid);
-                resp.status = STATUS_DEQ_FAIL_NO_SUCH_JOB;
+            pthread_mutex_lock(&mut_rj);
+            pthread_mutex_lock(&mut_q);
+
+            if (running_job && running_job->job.job_uuid == deq.job_uuid) {
+                RESMAND_ERROR("Refusing to dequeue currently running job\n")
+                resp.status = STATUS_DEQ_FAIL_JOB_CURRENTLY_RUNNING;
             } else {
-                RESMAND_INFO("Dequeueueueueing job %d\n", deq_job->job.job_uuid);
-                free_queued_job(deq_job);
+                queued_job *deq_job = remove_job(&q, deq.job_uuid);
+                if (!deq_job) {
+                    RESMAND_ERROR("Failed to dequeue job %d: not found in queue"
+                                  "\n", deq.job_uuid);
+                    resp.status = STATUS_DEQ_FAIL_NO_SUCH_JOB;
+                } else {
+                    RESMAND_INFO("Dequeueueueueing job %d\n", deq_job->job.job_uuid);
+                    free_queued_job(deq_job);
+                    resp.status = STATUS_OK;
+                }
+            }
+
+            pthread_mutex_unlock(&mut_q);
+            pthread_mutex_unlock(&mut_rj);
+
+            send_status(soc_client, &resp);
+            break;
+        case IPCREQ_RELEASE:
+            release_request rel = req.rel;
+
+            if (!running_job) {
+                RESMAND_INFO("Received manual release request by user %d, but "
+                             "server is not reserved. Nothing to do.\n",
+                             rel.uid)
+                resp.status = STATUS_OK;
+            } else {
+                RESMAND_INFO("Received manual release request by user %d (force: "
+                             "%d), cancelling current running job %d\n",
+                             rel.uid, rel.force, running_job->job.job_uuid);
+                pthread_mutex_lock(&mut_rj);
+                running_job->manually_released = 1;
+                pthread_mutex_unlock(&mut_rj);
                 resp.status = STATUS_OK;
             }
-        }
-
-        pthread_mutex_unlock(&mut_q);
-        pthread_mutex_unlock(&mut_rj);
-
-        send_status(soc_client, &resp);
-
-        disp_status();
-    } else if (req.req_type == IPCREQ_RELEASE) {
-        release_request rel = req.rel;
-
-        if (!running_job) {
-            RESMAND_INFO("Received manual release request by user %d, but "
-                         "server is not reserved. Nothing to do.\n",
-                         rel.uid)
-            resp.status = STATUS_OK;
-        } else {
-            RESMAND_INFO("Received manual release request by user %d (force: "
-                         "%d), cancelling current running job %d\n",
-                         rel.uid, rel.force, running_job->job.job_uuid);
-            pthread_mutex_lock(&mut_rj);
-            running_job->manually_released = 1;
-            pthread_mutex_unlock(&mut_rj);
-            resp.status = STATUS_OK;
-        }
-        send_status(soc_client, &resp);
-    } else {
-        RESMAND_ERROR("Invalid request type: %d\n", req.req_type);
-        close(soc_client);
-        return -1;
+            send_status(soc_client, &resp);
+            break;
+        default:
+            RESMAND_ERROR("Invalid request type: %d\n", req.req_type);
+            close(soc_client);
+            return -1;
     }
+
 _close:
     close(soc_client);
 
