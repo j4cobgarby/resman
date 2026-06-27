ccfe589a3a8076793dcf7cfe7fa7ed631700feba
Martin Hilgendorf <martin.hilgendorf@chalmers.se>
2026-06-28 01:14:30 +0200

    testclient.sh: Fix time slot job duration syntax
    Suffix is not optional

--- testclient.sh
+++ testclient.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Submitting 1 (time), while server idle --> should run right away"
-./build/resman time 1 -m "First (time)"
+./build/resman time 1s -m "First (time)"
 sleep 1
 
 echo "Submitting 2 (command), while server idle --> should run right away"
@@ -11,13 +11,13 @@ echo ""
 sleep 1
 
 echo "Submitting 3 (time), while server idle --> should run right away"
-./build/resman time 3 -m "Third (time)"
+./build/resman time 3s -m "Third (time)"
 
 echo "Submitting 4 (command), while server busy --> should go into queue and run eventually"
 ./build/resman run -m "Fourth (command)" -- bash -c 'echo Work: ; for i in {1..5} ; do echo -n $i, ; sleep 1 ; done'
 echo ""
 
 echo "Submitting 5 (time), but server busy --> should return an error message saying as much"
-./build/resman time 1 -m "Fifth (time, but server is busy)"
+./build/resman time 1s -m "Fifth (time, but server is busy)"
 
 echo ""
